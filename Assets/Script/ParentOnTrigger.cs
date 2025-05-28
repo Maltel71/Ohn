d56e4af268e06ee515154063a4ce44138ad0c584
Maltel71 <156253099+Maltel71@users.noreply.github.com>
2025-05-28 10:45:11 +0200

    shipcamerachanges

--- Assets/Script/ParentOnTrigger.cs
+++ Assets/Script/ParentOnTrigger.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class ParentOnTrigger : MonoBehaviour
 {
+    [Header("References")]
     public GameObject Player;
+    public GameObject freeLookCamera;
 
     private void Start()
     {
-        // Find the player using tag
-        Player = GameObject.FindGameObjectWithTag("Player");
+        // Find the player using tag if not assigned
+        if (Player == null)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -18,7 +23,14 @@ public class ParentOnTrigger : MonoBehaviour
         {
             // Parent the player to the parent object (which could be the boat)
             Player.transform.parent = transform.parent;
-            Debug.Log("Player parented to boat");
+
+            // Parent the freelook camera to the boat as well
+            if (freeLookCamera != null)
+            {
+                freeLookCamera.transform.parent = transform.parent;
+            }
+
+            Debug.Log("Player and FreeLook camera parented to boat");
         }
     }
 
@@ -28,7 +40,14 @@ public class ParentOnTrigger : MonoBehaviour
         {
             // Unparent the player
             Player.transform.parent = null;
-            Debug.Log("Player unparented from boat");
+
+            // Unparent the freelook camera
+            if (freeLookCamera != null)
+            {
+                freeLookCamera.transform.parent = null;
+            }
+
+            Debug.Log("Player and FreeLook camera unparented from boat");
         }
     }
 }
